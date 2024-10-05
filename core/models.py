41a6d739b9c6be9cdd00c8053cd87c4258c1bd50
kapil1831 <54210931+kapil1831@users.noreply.github.com>
2024-10-06 00:52:02 +0530

    added ExpenseGroup, Expense models (#1)
    * added ExpenseGroup, Expense models
    
    * added ExpenseGroup, GroupMember, Expense, Expense_participants models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ExpenseGroup(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    total_amount = models.DecimalField(null=True, blank=True, default=0, max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image_url = models.URLField()
+    
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return self.name
+    
+
+class GroupMember(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    group = models.ForeignKey('ExpenseGroup', related_name='group_members', on_delete=models.CASCADE)
+    is_admin = models.BooleanField(default=False)
+    
+    class Meta:
+        ordering = ['user']
+    
+    def __str__(self):
+        return self.user.username
+    
+class Expense(models.Model):
+    name = models.CharField(max_length=100)
+    expense_group = models.ForeignKey('ExpenseGroup', related_name='expense_group', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    description = models.TextField()
+    payer = models.ForeignKey('auth.User', related_name='payer', on_delete=models.CASCADE)
+    is_settled = models.BooleanField(default=False)    
+    class Meta:
+        ordering = ['date']
+
+    def __str__(self):
+        return self.name
+
+class ExpenseParticipant(models.Model):
+    expense = models.ForeignKey('Expense', related_name='expense_participants', on_delete=models.CASCADE)
+    participant_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    is_settled = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+    
+    
